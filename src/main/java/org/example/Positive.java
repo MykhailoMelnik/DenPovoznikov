b76a052039add568dc966f519373f5408f8e3045
mihajlomelnik <melnik8686@gmail.com>
2023-01-12 21:13:42 +0200

    You get an array of numbers, return the sum of all of the positives ones. Example [1,-4,7,12] => 1 + 7 + 12 = 20   Note: if there is nothing to sum, the sum is default to 0.

---
+++ src/main/java/org/example/Positive.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Positive {
+    // TODO: 12.01.2023  //    You get an array of numbers, return the sum of all of the positives ones.
+    //  Example [1,-4,7,12] => 1 + 7 + 12 = 20
+    //  Note: if there is nothing to sum, the sum is default to 0.
+    public static int sum(int[] arr) {
+
+        return ;
+    }
+}
